73f483d7aaadf8ecf09bfeeaa4e7439a9b94c408
alinadolbilova <genin.10@mail.ru>
2023-12-25 15:40:48 +0300

    add sorted_function

--- src/main.py
+++ src/main.py
@@ -12,3 +12,7 @@ def get_filtered_data(data_dict):
         elif data["state"] == "EXECUTED":
             date_list.append(data["date"][0:10])
     return date_list
+
+def get_sorted_list(date_list):
+    sorted_list = sorted(date_list, reverse=True)[0:5]
+    return sorted_list
