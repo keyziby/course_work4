d12a486c9659f945fd0f0b4cc5047da5580993eb
alinadolbilova <genin.10@mail.ru>
2023-12-25 15:39:58 +0300

    add function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
+import json
+
 def get_data(path):
     with open(path, encoding='utf-8') as f:
         data_dict = json.load(f)
         return data_dict
+def get_filtered_data(data_dict):
+    date_list = []
+    for data in data_dict:
+        if "from" not in data:
+            continue
+        elif data["state"] == "EXECUTED":
+            date_list.append(data["date"][0:10])
+    return date_list
