40230e0f7df21e0bbd36acace5aee6f102948616
alinadolbilova <genin.10@mail.ru>
2023-12-25 15:42:41 +0300

    add format_code_date

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ def get_data(path):
     with open(path, encoding='utf-8') as f:
         data_dict = json.load(f)
         return data_dict
+
 def get_filtered_data(data_dict):
     date_list = []
     for data in data_dict:
@@ -17,6 +18,7 @@ def get_sorted_list(date_list):
     sorted_list = sorted(date_list, reverse=True)[0:5]
     return sorted_list
 
+
 def number_format(name):
     if "счет" in name.lower():
         name_list = name.split()
@@ -29,3 +31,7 @@ def number_format(name):
                                                                                     4:6] + "**" + " " + "****" + " " + \
                         name_list[-1][-4:]
         return format_number
+
+def date_format(date):
+    format_date = ".".join(date.split("-")[::-1])
+    return format_date
